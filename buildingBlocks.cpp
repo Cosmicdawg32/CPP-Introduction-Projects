2caeec4775e5fe9d5ea1b146815f10ac66f846d5
Cosmicdawg32 <cosmicdawg32@gmail.com>
2025-10-06 13:10:38 -0400

    Chapter 1, Project 1

---
+++ buildingBlocks.cpp
@@ -0,0 +1,17 @@
+/*  Author: Nicholas Bleeker
+    Date: 10/6/2025
+    Project: Learn C++: Introduction (Ch.1, Project 1)
+    Description: Write a C++ program that displays the initials of the author's name in block letters.
+*/
+#include <iostream>
+
+int main() {
+  // NB
+  std::cout << "N   N BBBB \n";
+  std::cout << "NN  N B   B\n";
+  std::cout << "N N N B   B\n";
+  std::cout << "N  NN BBBB \n";
+  std::cout << "N   N B   B\n";
+  std::cout << "N   N B   B\n";
+  std::cout << "N   N BBBB \n";
+}
